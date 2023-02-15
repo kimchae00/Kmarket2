4fe87d1c619390302e7ad13e7ff818f47df1ee32
kcy013100 <kcy013100@naver.com>
2023-02-15 13:37:35 +0900

    2023/02/15 member 작업중

--- Kmarket2/src/main/java/kr/co/kmarket2/repository/MemberRepo.java
+++ Kmarket2/src/main/java/kr/co/kmarket2/repository/MemberRepo.java
@@ -1,5 +1,7 @@
 package kr.co.kmarket2.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import kr.co.kmarket2.entity.MemberEntity;
@@ -7,4 +9,5 @@ import kr.co.kmarket2.entity.MemberEntity;
 public interface MemberRepo extends JpaRepository<MemberEntity, String> {
 	
 	public int countByUid(String uid);
+	public List<MemberEntity> findMemberEntityByNameAndEmail(String name, String email);
 }
