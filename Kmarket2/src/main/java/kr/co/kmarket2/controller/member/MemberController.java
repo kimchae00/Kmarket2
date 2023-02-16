e02b4994a7ca4e5b95f98cf0cd9173a0fdd10c23
kcy013100 <kcy013100@naver.com>
2023-02-16 13:35:21 +0900

    23/02/16 member 자동로그인 구현중

--- Kmarket2/src/main/java/kr/co/kmarket2/controller/member/MemberController.java
+++ Kmarket2/src/main/java/kr/co/kmarket2/controller/member/MemberController.java
@@ -8,9 +8,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -26,6 +28,9 @@ public class MemberController {
 
 	@Autowired
 	private MemberService service;
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 	
 	@GetMapping("member/join")
 	public String join() {
@@ -82,6 +87,7 @@ public class MemberController {
 		return "/member/login";
 	}
 
+	/* 아이디찾기 */
 	@GetMapping("member/findId")
 	public String findId() {
 		return "/member/findId";
@@ -89,18 +95,54 @@ public class MemberController {
 	
 	@ResponseBody
 	@PostMapping("member/findId")
-	public String findId(@RequestParam("name") String name, @RequestParam("email") String email) {
-		if(service.findId(name, email)==null) {
-			return null;
-		}else {
-			return "/member/findId";
+	public Map<String, MemberVO> findId(Model model, @RequestParam("name") String name, @RequestParam("email") String email, HttpSession sess) throws Exception {
+		MemberVO vo = service.findId(name, email);
+		Map<String, MemberVO> map = new HashMap<>();
+		map.put("vo", vo);
+		if(vo != null) {
+			sess.setAttribute("member", vo);
 		}
-		
+		return map;
 	}
 	
 	@GetMapping("member/findIdResult")
-	public String findIdResult() {
+	public String findIdResult(Model model, HttpSession sess) {
+		MemberVO member = (MemberVO) sess.getAttribute("member");
+		//System.out.println("memberuid :" + member.getUid());
+		model.addAttribute("member", member);
 		return "/member/findIdResult";
+	}
+	
+	/* 비밀번호찾기(변경) */
+	@GetMapping("member/findPw")
+	public String findPw(HttpSession sess) {
+		sess.removeAttribute("member");
+		return "/member/findPw";
+	}
+	
+	@ResponseBody
+	@PostMapping("member/findPw")
+	public Map<String, Integer> findPw(String uid, String email) {
+		int result = service.findPw(uid, email);
+		Map<String, Integer> map = new HashMap<>();
+		map.put("result", result);
+		return map;
+	}
+	
+	@GetMapping("member/findPwChange")
+	public String findPwChange(Model model, String uid) {
+		model.addAttribute("uid",uid);
+		return "/member/findPwChange";
+	}
+	
+	@ResponseBody
+	@PostMapping("member/findPwChange")
+	public Map<String, Integer> findPwChange(@RequestParam("uid") String uid, @RequestParam("pass") String pass) {
+		pass = passwordEncoder.encode(pass);
+		int result = service.findPwChange(uid, pass);
+		Map<String, Integer> map = new HashMap<>();
+		map.put("result", result);
+		return map;
 	}
 	
 }
