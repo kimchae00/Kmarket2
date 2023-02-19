3c7883deaefae45505d00b296a873a1d22818fe8
hyun7494 <et009153@gmail.com>
2023-02-20 02:30:20 +0900

    qna 작업중

--- Kmarket2/src/main/java/kr/co/kmarket2/vo/ArticleVO.java
+++ Kmarket2/src/main/java/kr/co/kmarket2/vo/ArticleVO.java
@@ -24,10 +24,17 @@ public class ArticleVO {
 	public int file;
 	public int hit;
 	public String uid;
+	public String getUid() {
+		int first = uid.length()-3;
+		return uid.substring(0,first-1)+"****";
+	}
 	public String regip;
 	public String rdate;
 	public String getRdate() {
 		return rdate.substring(2,10);
 	}
+	
+	// 카테고리 이름 출력을 위해 추가
+	public String c2Name;
 	
 }
