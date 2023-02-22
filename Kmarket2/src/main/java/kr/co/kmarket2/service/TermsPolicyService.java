158015680bf66700706e4f1b435b4de1b4b28352
kcy013100 <kcy013100@naver.com>
2023-02-23 00:47:00 +0900

    2023/02/22 product/cart, termsPolicy 추가 작업

--- Kmarket2/src/main/java/kr/co/kmarket2/service/TermsPolicyService.java
+++ Kmarket2/src/main/java/kr/co/kmarket2/service/TermsPolicyService.java
@@ -1,9 +1,13 @@
 package kr.co.kmarket2.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import kr.co.kmarket2.dao.termsPolicyDAO;
+import kr.co.kmarket2.dao.TermsPolicyDAO;
+import kr.co.kmarket2.vo.ProdCate1VO;
+import kr.co.kmarket2.vo.ProdCate2VO;
 import kr.co.kmarket2.vo.TermsBuyerVO;
 import kr.co.kmarket2.vo.TermsFinanceVO;
 import kr.co.kmarket2.vo.TermsLocationVO;
@@ -12,12 +16,19 @@ import kr.co.kmarket2.vo.TermsSellerVO;
 
 
 @Service
-public class termsPolicyService {
+public class TermsPolicyService {
 	
 	@Autowired
-	private termsPolicyDAO dao;
+	private TermsPolicyDAO dao;
 	
-	public TermsBuyerVO selectTermsBuyer() {
+	public List<ProdCate1VO> selectCate1(){
+		return dao.selectCate1();
+	}
+	public List<ProdCate2VO> selectCate2(){
+		return dao.selectCate2();
+	}
+	
+	public List<TermsBuyerVO> selectTermsBuyer() {
 		return dao.selectTermsBuyer();
 	}
 	
